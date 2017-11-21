887a10ee12bd0f5b9c497e86cc486147c44d6505
mila <oktiabrina.sinkevich@gmail.com>
2017-11-22 02:18:56 +0300

    new

---
+++ src/Math/Array/RotateNLinearTimes.java
@@ -0,0 +1,29 @@
+package Math.Array;
+
+public class RotateNLinearTimes {
+
+    private static void reverse(int[] data, int d1, int d2) {
+        while (d1 < d2) {
+            int temp = data[d1];
+            data[d1] = data[d2];
+            data[d2] = temp;
+            d1++;
+            d2--;
+        }
+    }
+
+    private static void rotate(int[] data, int n) {
+        reverse(data, 0, data.length - 1);
+        reverse(data, 0, data.length - n - 1);
+        reverse(data, data.length - n, data.length - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4 };
+        rotate(array, 2);
+        for (int i : array)
+            System.out.print(i + ",");
+        System.out.println();
+
+    }
+}
